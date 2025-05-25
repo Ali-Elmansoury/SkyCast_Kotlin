cf9368c53c8fc6edf121508b1da4fbcea3c3335f
Ali-Elmansoury <ali.elmansoury21@gmail.com>
2025-05-25 19:41:44 +0300

    Update pojos to handle Room db entities

--- SkyCast/app/src/main/java/com/ities45/skycast/model/pojo/currentweather/CurrentWeatherResponse.kt
+++ SkyCast/app/src/main/java/com/ities45/skycast/model/pojo/currentweather/CurrentWeatherResponse.kt
@@ -1,17 +1,31 @@
 package com.ities45.skycast.model.pojo.currentweather
 
-import com.ities45.skycast.model.pojo.Clouds
-import com.ities45.skycast.model.pojo.Coord
-import com.ities45.skycast.model.pojo.Weather
-import com.ities45.skycast.model.pojo.Wind
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.ities45.skycast.model.local.entity.FavoriteLocationEntity
+import com.ities45.skycast.model.pojo.common.Clouds
+import com.ities45.skycast.model.pojo.common.Coord
+import com.ities45.skycast.model.pojo.common.Weather
+import com.ities45.skycast.model.pojo.common.Wind
 
+@Entity(
+    tableName = "current_weather",
+    foreignKeys = [ForeignKey(
+        entity = FavoriteLocationEntity::class,
+        parentColumns = ["locationId"],
+        childColumns = ["locationOwnerId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class CurrentWeatherResponse(
+    @PrimaryKey val id: Int,
+    val locationOwnerId: Int = 0, // Added for foreign key
     val base: String,
     val clouds: Clouds,
     val cod: Int,
     val coord: Coord,
     val dt: Int,
-    val id: Int,
     val main: Main,
     val name: String,
     val sys: Sys,
--- SkyCast/app/src/main/java/com/ities45/skycast/model/pojo/hourlyforecast/HourlyForecastItem.kt
+++ SkyCast/app/src/main/java/com/ities45/skycast/model/pojo/hourlyforecast/HourlyForecastItem.kt
@@ -1,10 +1,25 @@
 package com.ities45.skycast.model.pojo.hourlyforecast
 
-import com.ities45.skycast.model.pojo.Clouds
-import com.ities45.skycast.model.pojo.Weather
-import com.ities45.skycast.model.pojo.Wind
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.ities45.skycast.model.local.entity.FavoriteLocationEntity
+import com.ities45.skycast.model.pojo.common.Clouds
+import com.ities45.skycast.model.pojo.common.Weather
+import com.ities45.skycast.model.pojo.common.Wind
 
+@Entity(
+    tableName = "hourly_forecast",
+    foreignKeys = [ForeignKey(
+        entity = FavoriteLocationEntity::class,
+        parentColumns = ["locationId"],
+        childColumns = ["locationOwnerId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class HourlyForecastItem(
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    val locationOwnerId: Int = 0,
     val clouds: Clouds,
     val dt: Long,
     val dt_txt: String,
